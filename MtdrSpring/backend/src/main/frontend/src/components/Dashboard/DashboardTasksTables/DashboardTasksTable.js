1e0ec8b1639471266eaa5e3b2ddc35f481825c3f
julianesva <A01254679@tec.mx>
2025-05-01 14:35:24 -0600

    Correcting stuff

--- MtdrSpring/backend/src/main/frontend/src/components/Dashboard/DashboardTasksTables/DashboardTasksTable.js
+++ MtdrSpring/backend/src/main/frontend/src/components/Dashboard/DashboardTasksTables/DashboardTasksTable.js
@@ -13,7 +13,7 @@ export default function DashboardTasksTable({
   deleteItem,
   setIsHiddenRealHours,
 }) {
-   [isHidden, setIsHidden] = useState(false)
+   [isHidden, setIsHidden] = useState(false);
 
   function handleNextButton(event, task) {
     if (task.done == 0) {
